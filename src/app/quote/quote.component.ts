3d987dd1806a07014ba10a81f19b406ded531435
happiness <happinesskawinzi@gmail.com>
2019-08-18 23:38:05 +0300

    add dates according to when the quote was made

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import {Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote []= [
-   new Quote (1, 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw','Experiments are necessary for the experiences that create growth and new opportunities'),
-   new Quote (2, ' You must be the change you wish to see in the world — Gandhi','Live for what’s worth dying for, and leverage technology to create the world you wish to see.'),
-   new Quote (3, 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne','FOMO creates suffering, yet those who suffer, have FOMO.'),
-   new Quote (4, 'Life is really simple, but we insist on making it complicated — Confucius','Life is really simple, but social media has made it complicated.'),
+   new Quote (1, 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw','Experiments are necessary for the experiences that create growth and new opportunities',new Date(2019,3,14)),
+   new Quote (2, ' You must be the change you wish to see in the world — Gandhi','Live for what’s worth dying for, and leverage technology to create the world you wish to see.',new Date(2019,6,9)),
+   new Quote (3, 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne','FOMO creates suffering, yet those who suffer, have FOMO.',new Date(2019,1,12)),
+   new Quote (4, 'Life is really simple, but we insist on making it complicated — Confucius','Life is really simple, but social media has made it complicated.',new Date(2019,0,18)),
   ];
   toggleDetails(index){
     this.quotes[index].showDescription= !this.quotes[index].showDescription;
