76a413cdfda76604d35183b7093bb27ac6094cf5
happiness <happinesskawinzi@gmail.com>
2019-08-18 10:57:51 +0300

    moving the quote logic from the Appcomponent to the QuoteComponent

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes:Quote []= [
+    { id:1, name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw'},
+    {id:2, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+    {id:3, name: ' You must be the change you wish to see in the world — Gandhi'},
+    {id:4, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+  ];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
