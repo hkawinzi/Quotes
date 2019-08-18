96af31109dc3e8898c7b0a4dd0e21ffad825416e
happiness <happinesskawinzi@gmail.com>
2019-08-18 14:17:27 +0300

    the description of the quote is visible to the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import {Quote} from './quote';
 })
 
 export class AppComponent {
-  quotes:Quote []= [
-    { id:1, name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw'},
-    {id:2, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
-    {id:3, name: ' You must be the change you wish to see in the world — Gandhi'},
-    {id:4, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
-  ];
+  // quotes:Quote []= [
+  //   { id:1, name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw'},
+  //   {id:2, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+  //   {id:3, name: ' You must be the change you wish to see in the world — Gandhi'},
+  //   {id:4, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+  // ];
 }
