3eae90549e6f8211e7c4bbdc21272c6d6879bd33
happiness <happinesskawinzi@gmail.com>
2019-08-18 10:49:02 +0300

    creating and displaying quotes using the blueprint class 'quote'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ import {Quote} from './quote';
 })
 
 export class AppComponent {
-  quotes:Quote[];
-
-  
-    this.Quotes = ['A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw', 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne', ' You must be the change you wish to see in the world — Gandhi','He who fears he will suffer, already suffers because he fears — Michel De Montaigne']
-  
+  quotes:Quote []= [
+    { id:1, name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw'},
+    {id:2, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+    {id:3, name: ' You must be the change you wish to see in the world — Gandhi'},
+    {id:4, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+  ];
 }
