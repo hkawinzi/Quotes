a313aae65c95bc3f09595c5a9b05b5503f0a3942
happiness <happinesskawinzi@gmail.com>
2019-08-18 11:43:06 +0300

    adding description in the array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import {Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote []= [
-    { id:1, name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw'},
-    {id:2, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
-    {id:3, name: ' You must be the change you wish to see in the world — Gandhi'},
-    {id:4, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne'},
+    { id:1, name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw', description:'Experiments are necessary for the experiences that create growth and new opportunities.'},
+    {id:2, name: ' You must be the change you wish to see in the world — Gandhi',description:'Live for what’s worth dying for, and leverage technology to create the world you wish to see.'},
+    {id:3, name: 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne',description:'FOMO creates suffering, yet those who suffer, have FOMO.'},
+    {id:4, name: 'Life is really simple, but we insist on making it complicated — Confucius',description:'Life is really simple, but social media has made it complicated.'},
   ];
 
 
