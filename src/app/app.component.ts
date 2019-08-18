0c1724ec45733106e740eb29639d1161d850a6a2
happiness <happinesskawinzi@gmail.com>
2019-08-18 10:08:14 +0300

    this is my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  quotes:string[];
 
-  constructor(){
-    this.quotes = ['A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw', 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne', ' You must be the change you wish to see in the world — Gandhi','He who fears he will suffer, already suffers because he fears — Michel De Montaigne']
-  }
+export class AppComponent {
+  quotes:Quote[];
+
+  
+    this.Quotes = ['A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw', 'He who fears he will suffer, already suffers because he fears — Michel De Montaigne', ' You must be the change you wish to see in the world — Gandhi','He who fears he will suffer, already suffers because he fears — Michel De Montaigne']
+  
 }
